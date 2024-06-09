3ece535329d958770cdba7c864d9affce2973073
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-06-09 13:16:03 +0700

    Задисптчили addovies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,22 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from '../../common/apis/movieApi';
+import movieApi from "../../common/apis/movieApi";
 import { ApiKey } from "../../common/apis/movieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../movies/movieSlice";
 const Home = () => {
+  const dispatch = useDispatch();
+  const movieText = "Harry";
   useEffect(() => {
-    const movieText = 'Harry'
     const fecthMovies = async () => {
-      const response = await movieApi.get(`?ApiKey=${ApiKey}&s=${movieText}&type=movie`).catch((err)=>{
-        console.log('err', err)
-      });
-      console.log(response)
+      const response = await movieApi
+        .get(`?ApiKey=${ApiKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log("err", err);
+        });
+      dispatch(addMovies(response.data));
     };
-    fecthMovies()
+    fecthMovies();
   }, []);
   return (
     <>
